d759347d13dd8bac87959632dbe66cb78ec7f8b7
evaldodeoliveira <evaldodeoliveirasrs@yahoo.com.br>
2024-10-04 11:45:29 -0300

    Update 1_🏠_home.py

--- 1_🏠_home.py
+++ 1_🏠_home.py
@@ -19,10 +19,9 @@ st.markdown("# FIFA23 OFFICIAL DATASET! ⚽")
 st.sidebar.markdown("Desenvolvido por [Evaldo](https://www.linkedin.com/in/evaldodeoliveira/)")
 
 #btn == false
-btn = st.link_button("Acesse os dados no Kaggle")
+btn = st.button("Acesse os dados no Kaggle", 
+                "https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data")
 
-if btn:
-    webbrowser.open_new_tab("https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data")
 
 st.markdown(
     """
